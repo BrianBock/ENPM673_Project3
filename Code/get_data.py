616fe930e7ba0ae6a708f7c5bb2c9c35e87f6844
Justin Albrecht <jalbrec1@umd.edu>
2020-03-24 18:04:07 -0400

    refined roi picker for getting training data

--- Code/get_data.py
+++ Code/get_data.py
@@ -1,12 +1,7 @@
 import cv2 
-import numpy 
+import numpy as np
+import os
 
-def click_event(event, x, y, flags, param):
-    global clicked
-
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print(x, y)
-        clicked = True
 
 if __name__ == '__main__':
     filename = '../media/detectbuoy.avi'
@@ -14,36 +9,71 @@ if __name__ == '__main__':
     tot_frames = int(video.get(7))
     count = 0
     colors = ['yellow','orange','green']
-    font = cv2.FONT_HERSHEY_SIMPLEX 
+    bgr = {'yellow':(52,232,235),'orange':(52,137,235),'green':(52,235,86)}
+    for color in colors:
+        path = '../Training/'+color
+        print(path)
+        if os.path.exists(path):
+
+            for img in os.listdir(path):
+                if img.endswith('.png'):
+                    os.remove(path+'/'+img)
+        else:
+            os.mkdir(path)
+
+
+    font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+
     while video.isOpened():
         ret, frame = video.read()
-        h,w,ch = frame.shape
-        frames = {}
-        for color in colors:
-            frames[color] = frame.copy()
+        
         if ret:
+            h,w,ch = frame.shape
+            frames = {}
+            frame_w_text = frame.copy() 
+            text = 'Frame ' + str(count+1) + ' of ' + str(tot_frames)
+            cv2.putText(frame_w_text,text,(w-200,h-10),font,1,(0,0,0),2,cv2.LINE_AA)
+            cv2.putText(frame_w_text,text,(w-200,h-10),font,1,(255,255,255),1,cv2.LINE_AA)
+            bouy_count = 0
+
+            for color in colors:
+                color_frame = frame_w_text.copy()
+                cv2.rectangle(color_frame,(0,h-40),(40,h),bgr[color],-1)
+                frames[color]=color_frame
+
             for color in colors:
                 clicked = False
-                text1 = 'Click on the ' + color + ' bouy'
+                roi = []
+                text1 = 'Draw a rectangle arround the ' + color + ' bouy'
                 text2 = 'If the bouy is not present press ESC'
-                text3 = 'Frame ' + str(count) + ' of ' + str(tot_frames)
-                cv2.putText(frames[color],text1,(10,20),font,.6,(255,255,255),1,cv2.LINE_AA)
-                cv2.putText(frames[color],text2,(10,40),font,.6,(255,255,255),1,cv2.LINE_AA)
-                cv2.putText(frames[color],text3,(10,h-10),font,.6,(255,255,255),1,cv2.LINE_AA)
-                cv2.imshow('Frame',frames[color])
-                cv2.setMouseCallback("Frame", click_event)
                 
-                while clicked == False:
-                    key = cv2.waitKey(1)
-                    if key == 27:
-                        print("ESC was pressed")
-                        break
+                cv2.putText(frames[color],text1,(10,20),font,1,(0,0,0),2,cv2.LINE_AA)
+                cv2.putText(frames[color],text1,(10,20),font,1,(255,255,255),1,cv2.LINE_AA)
+                cv2.putText(frames[color],text2,(10,40),font,1,(0,0,0),2,cv2.LINE_AA)
+                cv2.putText(frames[color],text2,(10,40),font,1,(255,255,255),1,cv2.LINE_AA)
+                
 
-                    elif key == ord('q'):
-                        video.release()
-                        break
-                if key == ord('q'):
-                    break
+                cv2.imshow('Frame',frames[color])
+                (x,y,w,h) = cv2.selectROI("Frame", frames[color], fromCenter=False,showCrosshair=False)
+
+                if w == 0 or h == 0:
+                    continue
+
+                else:
+                    bouy_count += 1
+                    bouy = frame[y:y+h,x:x+w]
+                    mask = np.zeros(bouy.shape[:2],np.uint8)
+                    cv2.ellipse(mask,(w//2,h//2),(w//2,h//2),0,0,360,255,-1)
+                    bouy = cv2.bitwise_and(bouy,bouy,mask = mask)
+                    # cv2.imshow('Bouy',bouy) 
+                    # cv2.waitkey(0)
+                    # cv2.destroyWindow('Bouy')
+                    path = '../Training/'+color+'/'+str(count)+'.png'
+                    cv2.imwrite(path,bouy)
+                    
+            if bouy_count == 0:
+                video.release()
+                cv2.destroyAllWindows()
 
             count +=1
         else:
