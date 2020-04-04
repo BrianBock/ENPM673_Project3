372f7fb24f133ba753c51913235f8ebb3a8ca2a8
Brian Bock <bbock@terpmail.umd.edu>
2020-04-04 11:04:07 -0400

    fixed string data so it plots properly

--- Code/time_plotter.py
+++ Code/time_plotter.py
@@ -15,12 +15,24 @@ filepath="diff_times.txt"
 dataFile = open(filepath,"r")
 
 data=[]
+diff_list=[]
+time_s_list=[]
 
-for line in dataFile:
+for i,line in enumerate(dataFile):
 	line=line[:-1] #lines end with "\n", remove
 	diff,time=line.split(',')
-	print(diff)
-	data.append(dtime.strptime(time, "%H:%M:%S.%f"))#convert the strings to datetime objects
+	diff_list.append(float(diff))
+	# print(diff)
+	# timestamp=time[11:]
+	
+	timestamp=dtime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")#convert the strings to datetime objects
+	# print(timestamp)
+	time_s  = timestamp.timestamp()-1585952995.141063
+	# print(time_s)
+	time_s_list.append(time_s)
+
+	data.append(timestamp)
+	# print(mytime[i])
 
 delta_time=[]
 
@@ -31,13 +43,15 @@ for i in range (0,len(data)-1):
 	#print(delta_ms)
 
 
-plt.hist(data,20,ec='black')
-plt.plot(time,diff)
-plt.xlabel('Processing Time (ms)')
+# plt.hist(data,20,ec='black')
+plt.plot(time_s_list,diff_list,"red")
+plt.xlabel('Time (s)')
 plt.ylabel('Difference')
 plt.title("Processing Time per Iteration")
-
-print(statistics.mean(delta_time))
+plt.yscale('log')
+# plt.xscale('log')
+plt.grid('both')
+# print(statistics.mean(delta_time))
 
 # # Create a new figure
 # plt.figure()
